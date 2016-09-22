7bf5165f0e5310a22cedb62fa32c4d685f09185c
Garðar Þorsteinsson <Garar orsteinsson>
2016-09-22 10:32:32 +0000

    Remove Canvas Menu

--- Canvas/CanvasMenu.cs
+++ Canvas/CanvasMenu.cs
@@ -16,20 +16,20 @@ namespace Canvas
         {
             //Listen for the ApplicationInit event which then allows us to bind to the
             //HttpApplication events.
-            TreeControllerBase.MenuRendering += TreeControllerBase_MenuRendering;
+            //TreeControllerBase.MenuRendering += TreeControllerBase_MenuRendering;
         }
 
 
         public void TreeControllerBase_MenuRendering(TreeControllerBase sender, MenuRenderingEventArgs e)
         {
-            if (sender.TreeAlias == "content")
-            {
-                var i = new MenuItem("editCanvas", "Edit in Canvas");
-                i.Icon = "edit";
-                i.LaunchDialogUrl("/umbraco/canvas?pageId=" + e.NodeId,"Edit in Canvas");
+            //if (sender.TreeAlias == "content")
+            //{
+                //var i = new MenuItem("editCanvas", "Edit in Canvas");
+                //i.Icon = "edit";
+                //i.LaunchDialogUrl("/umbraco/canvas?pageId=" + e.NodeId,"Edit in Canvas");
 
-                e.Menu.Items.Insert(3,i);
-            }
+                //e.Menu.Items.Insert(3,i);
+            //}
         }
         
     }
